e49b584d85c3da6a796d437439665313aa4c4480
GuilhermeTakemoto <guilhermetakemoto2@gmai.com>
2024-08-24 10:32:14 -0300

    :tada: Commit inicial

---
+++ Rezervei/Contexts/DBContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Rezervei.Contexts.builders;
+using Rezervei.Object.Models;
+
+namespace Rezervei.Contexts
+{
+    public class DBContext : DbContext
+    {
+        //mapeamento Relacional dos Objetos no Banco de dados
+        public DBContext(DbContextOptions<DBContext> options) : base(options) { }
+        //Conjunto:usuario
+        public DbSet<UserModel> Users { get; set; }
+
+        // Fluent API
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            UserBuilder.Build(modelBuilder);
+        }
+    }
+}
---
+++ Rezervei/Object/DTOs/Mappings/MappingProfile.cs
@@ -0,0 +1,18 @@
+﻿
+
+using AutoMapper;
+using Rezervei.Object.DTOs.Entities;
+using Rezervei.Object.Models;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Rezervei.Object.DTOs.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile() { 
+        
+        CreateMap<UserDTO, UserModel>().ReverseMap();
+
+        }
+    }
+}
